735df491894d5be73b98a5020b7eb145d2a0f0d9
pytechlady <username@Usernames-MacBook-Air.local>
2024-01-24 23:55:25 +0000

    static files

--- analytics/urls.py
+++ analytics/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.index, name="home"),
+    path("", views.index, name="home"),
     path("upload/", views.upload, name="upload"),
     path("upload-form/", views.upload_form, name="upload-form"),
     path('get_column_names/', views.get_column_names, name='get_column_names'),
--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,9 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / '/staticfiles'
+
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
